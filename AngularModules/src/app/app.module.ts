57dea26b2553aa2aa89609f1343213f807f3ed06
LucaEvaroni <info@luca.evaroni.name>
2020-09-05 00:44:03 +0200

    rifiniture

--- AngularModules/src/app/app.module.ts
+++ AngularModules/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DomainModule }from './modules/domain/domain.module';
+import { DomainRoutingModule } from './modules/domain/domain-routing.module';
 
 @NgModule({
   declarations: [
--- AngularModules/src/app/modules/domain/domain.module.ts
+++ AngularModules/src/app/modules/domain/domain.module.ts
@@ -5,10 +5,11 @@ import { DomainRoutingModule } from './domain-routing.module';
 import { IndexComponent } from './pages/index/index.component';
 import { ComponentOneComponent } from './components/component-one/component-one.component';
 import { ComponentTwoComponent } from './components/component-two/component-two.component';
+import { TestComponent } from './components/test/test.component';
 
 
 @NgModule({
-  declarations: [IndexComponent, ComponentOneComponent, ComponentTwoComponent],
+  declarations: [IndexComponent, ComponentOneComponent, ComponentTwoComponent, TestComponent],
   imports: [
     CommonModule,
     DomainRoutingModule
